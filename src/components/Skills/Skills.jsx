29640268a00a661c6e1096230bb71dee8db812a2
Tejas Vermani <tvermani3@gatech.edu>
2022-10-23 17:38:13 -0400

    mobile css for projects and contact me

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,6 +1,80 @@
 import React from 'react'
 import './Skills.css'
-import './SkillBar.css'
+
+import {FaJava, FaPython, FaReact, } from 'react-icons/fa'
+import {GrOracle} from 'react-icons/gr'
+import {SiAssemblyscript, SiCplusplus} from 'react-icons/si'
+
+const data = [
+  {
+      id: "1",
+      icon: <FaJava />,
+      skill: "Java",
+      level: "90"
+  },
+  {
+      id: "2",
+      icon: <FaPython />,
+      skill: "Python",
+      level: "90"
+  }, 
+  {
+      id: "3",
+      icon: <GrOracle />,
+      skill: "JSP",
+      level: "90%"
+  },
+  {
+      id: "4",
+      icon: <FaReact />,
+      skill: "React JS",
+      level: "90%"
+  },
+  {
+      id: "5",
+      icon: <SiAssemblyscript />,
+      skill: "Assembly",
+      level: "90%"
+  },
+  {
+      id: "6",
+      icon: <SiCplusplus />,
+      skill: "C, C++",
+      level: "10%"
+  },
+
+  
+  {
+      id: "11",
+      icon: <FaJava />,
+      skill: "Stocks",
+      level: "95%"
+  },
+  {
+      id: "12",
+      icon: <FaJava />,
+      skill: "Fixed Income",
+      level: "95%"
+  },
+  {
+      id: "13",
+      icon: <FaJava />,
+      skill: "ETFs/Mutual Funds",
+      level: "95%"
+  },
+  {
+      id: "14",
+      icon: <FaJava />,
+      skill: "Options",
+      level: "95%"
+  },
+  {
+      id: "15",
+      icon: <FaJava />,
+      skill: "Futures/",
+      level: "95%"
+  }
+]
 
 const Progress = ({done}) => {
 	const [style, setStyle] = React.useState({});
