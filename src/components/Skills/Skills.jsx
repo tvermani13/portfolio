14120980380194a13f716f5fb5ce3371e00cfba0
Tejas Vermani <tvermani3@gatech.edu>
2022-10-31 14:44:28 -0400

    misc

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -122,7 +122,7 @@ const Skills = () => {
 
       <h3 className='title__text'>Financial Instruments</h3>
       {
-        financialSkills.map(({icon, skill, level}) => {
+        financialSkills.map(({skill, level}) => {
           return(
             <div className="skill-box">
               <span className="title__text"> {skill} </span>
@@ -139,68 +139,3 @@ const Skills = () => {
 }
 
 export default Skills
-
-    // Programming skills: java, python, c, assembly, reactjs, JSP
-    // Investment skills: Stocks, bonds, ETFs, options, spreads, mutual funds, etc. (check resume)
-
-
-
-    // <div className="skill-box">
-    //       <span className="title">Java</span>
-    //       {/* <div className="skill-bar">
-    //         <span className="skill-per java">
-    //           <span className="tooltip">80%</span>
-    //         </span>
-    //       </div> */}
-    //       <Progress done="80" />
-    //     </div>
-
-    //     <div className="skill-box">
-    //       <span className="title">Python</span>
-    //       {/* <div className="skill-bar">
-    //         <span className="skill-per python">
-    //           <span className="tooltip">70%</span>
-    //         </span>
-    //       </div> */}
-    //       <Progress done="70" />
-    //     </div>
-
-    //     <div className="skill-box">
-    //       <span className="title">React JS</span>
-    //       {/* <div className="skill-bar">
-    //         <span className="skill-per react">
-    //           <span className="tooltip">60%</span>
-    //         </span>
-    //       </div> */}
-    //       <Progress done="60" />
-    //     </div>
-
-    //     <div className="skill-box">
-    //       <span className="title">JSP</span>
-    //       {/* <div className="skill-bar">
-    //         <span className="skill-per jsp">
-    //           <span className="tooltip">70%</span>
-    //         </span>
-    //       </div> */}
-    //       <Progress done="70" />
-    //     </div>
-
-    //     <div className="skill-box">
-    //       <span className="title">Assembly</span>
-    //       {/* <div className="skill-bar">
-    //         <span className="skill-per assembly">
-    //           <span className="tooltip">40%</span>
-    //         </span>
-    //       </div> */}
-    //       <Progress done="40" />
-    //     </div>
-
-    //     <div className="skill-box">
-    //       <span className="title">C/C++</span>
-    //       {/* <div className="skill-bar">
-    //         <span className="skill-per c">
-    //           <span className="tooltip">20%</span>
-    //         </span>
-    //       </div> */}
-    //       <Progress done="20" />
-    //     </div>
