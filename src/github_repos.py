08c445328078ccc50a26d042b79158c2c2b936a5
Tejas Vermani <tvermani3@gatech.edu>
2023-07-23 23:00:14 -0400

    added credentials to gitignore

---
+++ src/github_repos.py
@@ -0,0 +1,43 @@
+import requests
+import credentials
+
+from github import Github
+from github import Auth
+
+# with python module for accessing Github REST API
+
+auth = Auth.Token(credentials.GITHUB_API_KEY)
+g = Github(auth=auth)
+
+def fetch_all_repositories():
+    for repo in g.get_user().get_repos():
+        print(repo)
+        # print(repo.name)
+        
+# Using ChatGPT generated code
+        
+username = credentials.GITHUB_USERNAME
+access_token = credentials.GITHUB_API_KEY
+
+def fetch_public_repositories():
+    url = f"https://api.github.com/users/{username}/repos"
+    headers = {"Authorization": f"token {access_token}"}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        repositories = response.json()
+        return repositories
+    else:
+        raise Exception(f"Failed to fetch repositories. Status code: {response.status_code}\n{response.text}")
+    
+# Execution:
+
+if __name__ == "__main__":
+    try:
+        repositories = fetch_public_repositories()
+        # repositories = fetch_all_repositories()
+        
+        for repo in repositories:
+            print(f"Repository Name: {repo['name']}, URL: {repo['html_url']}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
