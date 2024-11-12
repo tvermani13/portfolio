00ce003249ffffe7f3d98cca8f584c43de22a2d5
Tejas Vermani <tejasvermani@gmail.com>
2024-11-12 17:00:23 -0500

    updated resume

--- src/components/Header/CTA.jsx
+++ src/components/Header/CTA.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import NewResume from '../../assets/Resume.pdf'
+import Resume from '../../assets/Resume.pdf'
 
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href={NewResume} download className='btn'>Download resume</a>
+        <a href={Resume} download className='btn'>Download resume</a>
         <a href="#Contacts" className='btn-primary'>Contact me</a>
     </div>
   )
