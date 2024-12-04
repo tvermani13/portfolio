49c2beb075c29eee84bfb9b17d7e90d5fa609b60
Tejas Vermani <tejasvermani@gmail.com>
2024-12-04 13:16:14 -0500

    fixed projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -6,108 +6,96 @@ import IMG2 from '../../assets/ACT-logo.jpg'
 import IMG3 from '../../assets/CampusDiscovery.png'
 import IMG4 from '../../assets/Frintz-logo.png'
 import IMG5 from '../../assets/React.png'
-
-
-// import { Swiper, SwiperSlide } from "swiper/react";
-
-// import "swiper/css";
-// import "swiper/css/pagination";
-
-
-// import { Pagination } from "swiper";
-
-const data = [
-  {
-    id: 0,
-    image: IMG1,
-    title: 'Predicting Credit Risk (in progress)',
-    github: 'https://github.com/Predicting-Credit-Risk/predicting-credit-risk',
-    demo: 'https://predicting-credit-risk.streamlit.app'
-  },
-  {
-    id: 1,
-    image: IMG2,
-    title: 'ACT Driving Simulator',
-    github: 'https://github.com/tvermani13/VIP-Projects'
-    // demo: 'https://github.com'
-  },
-  {
-    id: 2,
-    image: IMG3,
-    title: 'Campus Discovery',
-    github: 'https://github.gatech.edu/Team-31-TUHAJS/Campus-Discovery'
-    // demo: 'https://github.com'
-  }, 
-  {
-    id: 3,
-    image: IMG0,
-    title: 'Coaching and Consulting (in progress)',
-    github: 'https://github.com/tvermani13/coaching-consulting'
-    // demo: 'https://github.com'
-  }, 
-  {
-    id: 4,
-    image: IMG4,
-    title: 'Frintz',
-    github: 'https://github.com'
-    // demo: 'https://frintz.com/'
-  }, 
-
-  {
-    id: 6,
-    image: IMG5,
-    title: 'React Portfolio (this site)',
-    github: 'https://github.com/tvermani13/portfolio'
-    // demo: 'https://tvermani13.github.io/portfolio/'
-  }  
-]
+import IMG6 from '../../assets/Bank.png'
+import IMG7 from '../../assets/DB.png'
 
 const Projects = () => {
+  const projects = [
+    {
+      id: 0,
+      image: IMG1,
+      title: 'Predicting Credit Risk (in progress)',
+      github: 'https://github.com/Predicting-Credit-Risk/predicting-credit-risk',
+      hasDemo: true,
+      demo: 'https://predicting-credit-risk-jysg5sq5rq6dbfuznc5cqz.streamlit.app'
+    },
+    {
+      id: 1,
+      image: IMG2,
+      title: 'ACT Driving Simulator',
+      github: 'https://github.com/tvermani13/VIP-Projects',
+      hasDemo: false,
+      // demo: 'https://github.com'
+    },
+    {
+      id: 2,
+      image: IMG3,
+      title: 'Campus Discovery',
+      github: 'https://github.gatech.edu/Team-31-TUHAJS/Campus-Discovery',
+      hasDemo: false,
+      // demo: 'https://github.com' ?? DEPLOY?
+    }, 
+    {
+      id: 3,
+      image: IMG0,
+      title: 'Coaching and Consulting (in progress)',
+      github: 'https://github.com/tvermani13/coaching-consulting',
+      hasDemo: true,
+      demo: 'https://coaching-consulting.vercel.app'
+    },
+    {
+      id: 5,
+      image: IMG5,
+      title: 'React Portfolio (this site)',
+      github: 'https://github.com/tvermani13/portfolio',
+      hasDemo: true,
+      demo: 'https://tvermani13.github.io/portfolio/'
+    },
+    {
+      id: 6,
+      image: IMG6,
+      title: 'Neural Net Income',
+      github: "https://github.com/tvermani13/liquidity-optimizer",
+      hasDemo: true,
+      demo: "https://liquidity-optimizer.vercel.app"
+    },
+    {
+      id: 7,
+      image: IMG7,
+      title: 'Databases Fullstack',
+      github: "https://github.com/tvermani13/databases-fullstack",
+      hasDemo: true,
+      demo: "https://databases-fullstack.vercel.app"
+    }
+  ];
+
   return (
     <section id='Projects'>
       <h5>My Recent Work</h5>
       <h2>Projects</h2>
       <div className='portfolio__container'>
-        {/* <Swiper className="container project__container" pagination={true} modules={[Pagination]} > */}
-        {
-          data.map(({id, image, title, github}) => {
+        {projects.map(({id, image, title, github, hasDemo, demo}) => {
             return(
-              // <SwiperSlide key={id} className="container portfolio__item">
               <article className="container portfolio__item">
                 <div className="portfolio__item-image">
                   <img src={image} alt={title} />
                 </div>
                 <h3> {title} </h3>
                 <div className="portfolio__item-cta">
-                  <a href= {github} className="btn" rel="noreferrer" target='_blank'>Github</a> {/* Insert links to specific repos on github*/}
-                                    
+                  <a href= {github} className="btn" rel="noreferrer" target='_blank'>Github</a>
+                  {hasDemo && (
+                    <a href={demo} target="_blank" rel="noopener noreferrer">
+                      Demo
+                    </a>
+                  )}
                 </div>
               </article>
-            // </SwiperSlide>
-            // <a href= {demo} className="btn btn-primary" rel="noreferrer" target='_blank'>Live Demo</a> {/* Insert live demo link if applicable */} 
-
             )
           })
         }
-
-      {/* </Swiper> */}
       </div>
     </section>
   )
 }
 
 export default Projects
-
-/* 
-
-Projects: 
-
-Frintz (no github)
-CSV to spreadsheet (github)
-2340 Campus Discover (github)
-VIP (github)
-Web-scraper  WORK ON THIS WITH OR WITHOUT HENOK
-
-FIGURE OUT HOW TO FIX ALIGNMENTS WITH TEXT AND IMAGES - SAME SIZE IMAGES???
-
-*/
